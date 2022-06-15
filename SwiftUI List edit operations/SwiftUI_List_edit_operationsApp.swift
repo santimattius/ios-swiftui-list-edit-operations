9e7f309fd2a2c1847b8624153212053309d4d63e
Santiago Mattiauda <santimattius@gmail.com>
2022-06-15 19:31:10 -0300

    Initial Commit

---
+++ SwiftUI List edit operations/SwiftUI_List_edit_operationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_List_edit_operationsApp.swift
+//  SwiftUI List edit operations
+//
+//  Created by Santiago Mattiauda on 15/6/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_List_edit_operationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
